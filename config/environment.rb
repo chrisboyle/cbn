8799c2fcef54bcbdbe2e42c781530be3536e3418
Chris Boyle <chris@boyle.name>
2010-04-11 01:33:31 +0100

    Add titles; fix the tree.

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,9 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
+SITE_NAME = 'Chris Boyle'
+TAGLINE = 'Cambridge-based software engineer'
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
