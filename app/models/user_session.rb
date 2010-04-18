98ac706fd34b009250033264c81c45d65775be28
Chris Boyle <chris@boyle.name>
2010-04-18 17:50:10 +0100

    Switch to agibralter's FB connect fork for the nifty use-FB-or-not switch.

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -2,4 +2,11 @@ class UserSession < Authlogic::Session::Base
 	auto_register
 	# Don't spam the DB
 	last_request_at_threshold 1.minute
+	facebook_auth_if :auth_using_fb
+
+	private
+
+	def auth_using_fb
+		controller.params[:auth_using_fb].present?
+	end
 end
