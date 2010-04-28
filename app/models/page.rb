6d4c627fb1421fff2c1f89c5bab9061c00e64b3e
Chris Boyle <chris@boyle.name>
2010-04-28 22:04:27 +0100

    Validate HAML.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,12 @@
 class Page < ActiveRecord::Base
 	validates_presence_of :title, :body
+	validates_each :body do |record, attr, value|
+		begin
+			Haml::Engine.new(value, :format => :html5).render
+		rescue Haml::Error => e
+			record.errors.add attr, "line #{e.line || 'unknown'}: #{e.message}"
+		end
+	end
 	has_many :comments
 	def year
 		created_at.year.to_s
