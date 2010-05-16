b0ebc26d29b7ae3a7999de71e116b7bf382a6e9c
Chris Boyle <chris@boyle.name>
2010-05-16 17:03:54 +0100

    Fix Facebook login (issue #20).

--- app/helpers/user_sessions_helper.rb
+++ app/helpers/user_sessions_helper.rb
@@ -4,21 +4,20 @@ module UserSessionsHelper
 	end
 	def login_button_action(name, url)
 		update_page do |page|
-			t = page[:user_session_openid_identifier]
 			if name == 'Facebook'
 				page.call 'FB.Connect.requireSession' do |p|
-					page[:auth_using_fb].value = 1
-					page[:user_session_submit].click
+					p[:auth_using_fb].value = 1
+					p[:new_user_session].submit
 				end
 			elsif name == 'Twitter'
 				page[:login_with_oauth].value = 1
-				page[:user_session_submit].click
+				page[:new_user_session].submit
 			else
-				t.value = url
+				page[:user_session_openid_identifier].value = url
 				if /username/.match url
 					page.call 'selectTextField', :user_session_openid_identifier, $~.offset(0)
 				else
-					page[:user_session_submit].click
+					page[:new_user_session].submit
 				end
 			end
 		end
