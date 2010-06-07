adab274e38ea9e31d1f377c7a74919009d30f138
Chris Boyle <chris@boyle.name>
2010-06-07 02:25:36 +0100

    An unapproved comment is insufficient to cause its deleted parent to be visible.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,7 +8,7 @@ class Comment < ActiveRecord::Base
 	named_scope :visible_to, lambda {|user| {:conditions => (user and user.role_symbols.include? :admin) ? nil : {:deleted => false, :approved => true}}}
 
 	def is_visible_to?(user)
-		(user and user.role_symbols.include? :admin) or (approved and (not deleted or descendants.any? {|c| not c.deleted}))
+		(user and user.role_symbols.include? :admin) or (approved and (not deleted or descendants.any? {|c| c.approved and not c.deleted}))
 	end
 
 	def parent_deleted
