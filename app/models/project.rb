f243e33dc9b4d2f0074f1b36154f350491ea2cf5
Chris Boyle <chris@boyle.name>
2010-06-13 18:24:10 +0100

    Tidy up project/post rendering; validate HAML in projects (issue #34).

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,17 @@ class Project < ActiveRecord::Base
 	validates_presence_of :title, :body
 	STATUSES = %w( idea planning active maintenance abandoned )
 	validates_inclusion_of :status, :in => STATUSES
+	validates_each :body do |record, attr, value|
+		begin
+			contr = ProjectsController.new()
+			contr.response = ActionController::Response.new()
+			scope = ActionView::Base.new(["#{RAILS_ROOT}/app/views/projects","#{RAILS_ROOT}/app/views"], {}, contr)
+			scope.template_format = 'html'
+			Haml::Engine.new(value, :format => :html5).render(scope)
+		rescue Exception => e
+			record.errors.add attr, "line #{(e.respond_to? :line) && e.line || 'unknown'}: #{e.message}".sub('%','% ')
+		end
+	end
 
 	def to_param
 		name
