58f7a9e6310441e9a533570768d18c2343f083fb
Chris Boyle <chris@boyle.name>
2010-06-13 16:28:38 +0100

    Add redirects (issue #32).

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,26 @@ ActionController::Routing::Routes.draw do |map|
 	map.resources     :comments, :except => [:new,:create], :member => {:reply => :get, :approve => :post, :trust => :post, :disapprove => :post}
 	map.resources     :projects
 	map.resources     :acts_as_taggable_on_tags, :as => :tags, :only => [:index,:show], :controller => :tags
+
+	map.redirect      'blog', '/'
+	map.redirect      'journal', '/'
+	map.redirect      'rss', '/feed'
+	map.redirect      '3yp', '/projects/c4'
+	map.redirect      'amazon', 'https://www.amazon.co.uk/gp/pdp/profile/A14ZX1C5G75VLP?ie=UTF8'
+	map.redirect      'calendar', 'http://www.google.com/calendar/embed?src=chris.boyle.name%40gmail.com&ctz=Europe/London'
+	map.redirect      'comics', 'http://www.google.com/reader/shared/user/17429808451195384076/label/comics'
+	map.redirect      'ebay', 'http://feedback.ebay.com/ws/eBayISAPI.dll?ViewFeedback&userid=chrisboyle'
+	map.redirect      'facebook', 'http://www.facebook.com/shortcipher'
+	map.redirect      'insanejournal', 'http://shortcipher.insanejournal.com/86249.html'
+	map.redirect      'lastfm', 'http://last.fm/user/shortcipher'
+	map.redirect      'linkedin', 'http://www.linkedin.com/in/shortcipher'
+	map.redirect      'livejournal', 'http://shortcipher.livejournal.com/71821.html'
+	map.redirect      'photos', 'http://picasaweb.google.com/chris.boyle.name'
+	map.redirect      'picasa', 'http://picasaweb.google.com/chris.boyle.name'
+	map.redirect      'steam', 'http://steamcommunity.com/id/kerneloops'
+	map.redirect      'twitter', 'http://twitter.com/c_boyle'
+	map.redirect      'wishlist', 'http://www.amazon.co.uk/gp/registry/wishlist/RC97AUMCBBGA?reveal=unpurchased&filter=all&sort=priority&layout=standard'
+	map.redirect      'xmms-arts', 'http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=456745'
+
 	map.resources     :static_pages, :except => :index, :member_path => ':name'
 end
