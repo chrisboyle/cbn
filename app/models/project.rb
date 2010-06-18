f8b680f645670796fb8d585d2602b148b0a409ca
Chris Boyle <chris@boyle.name>
2010-06-18 19:05:32 +0100

    Show file sizes of downloads (issue #39) and tweak extensions.

--- app/models/project.rb
+++ app/models/project.rb
@@ -31,7 +31,7 @@ class Project < ActiveRecord::Base
 		bin, src, sig = [], [], {}
 		files.each do |f|
 			case f.extname
-			when *%w(.gz .tgz .tbz .rb .py .pl .js .c .cpp) then src << f
+			when *%w(.gz .bz2 .tgz .tbz .rb .py .pl .js .c .cpp) then src << f
 			when *%w(.apk .deb .ko .so) then bin << f
 			when *%w(.asc) then sig[f.to_s[0..-5]] = f
 			end
